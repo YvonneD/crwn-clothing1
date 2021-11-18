f8624dd966fee46c23e34fa0bfa9e6c552268212
Yifang Di <diyifang@ymail.com>
2021-11-18 16:13:04 -0500

    firebase for google sign-in

--- src/App.js
+++ src/App.js
@@ -3,18 +3,43 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
-import "./App.css";
+import SignInAndSignUp from "./pages/shop/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth } from "./components/firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) =>
+      this.setState({ currentUser: user })
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/shop/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../../components/sign-in/sign-in.component";
+
+import "./sign-in-and-sign-up.component.scss";
+
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;
